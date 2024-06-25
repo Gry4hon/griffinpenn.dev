7b88ff1a6a4a5689b64796d0161e09b665fd8216
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2024-06-24 22:30:11 -0700

    Landing Page Progress
    Just getting started with laying out the essentials for the landing page, and getting rid of the basic Vite template stuff.

---
+++ griffinpennportfolio/src/Components/LandingPageComponents/LandingBody.tsx
@@ -0,0 +1,13 @@
+import "./LandingStyles/LandingBodyStyles.css";
+
+function LandingBody() {
+  return (
+    <>
+      <body>
+        <h1>Hello Worm</h1>
+      </body>
+    </>
+  );
+}
+
+export default LandingBody;
