989e41ff22acabdab6164e86e0df15ce2dccc809
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2024-07-28 08:21:10 -0700

    Did not look good on Mac!
    The style of the first few components where way off for what I envisioned for monitors of standard size...

--- griffinpennportfolio/src/Components/LandingPageComponents/LandingBody.tsx
+++ griffinpennportfolio/src/Components/LandingPageComponents/LandingBody.tsx
@@ -6,25 +6,27 @@ function LandingBody() {
   return (
     <>
       <div id="landing-body">
-        <div className="container landing-elements">
-          <div className="landing-text">
-            <p>Greetings, my name is</p>
-            <p id="first-name">
-              <b>Griffin Penn</b>
-            </p>
-            <p id="software-dev">
-              I am a <b>Software Developer</b> based out of{" "}
-              <b>Seattle, Washington</b>
-            </p>
-            <p id="port">
-              Take a look at my <b>portfolio</b> below!
-            </p>
-          </div>
-          <div className="arrow-styles">
-            <img src={arrow} />
-          </div>
-          <div className="landing-image">
-            <img src={animatedMe} />
+        <div className="container">
+          <div className="landing-elements">
+            <div className="landing-text">
+              <p>Greetings, my name is</p>
+              <p id="first-name">
+                <b>Griffin Penn</b>
+              </p>
+              <p id="software-dev">
+                I am a <b>Software Developer</b> based out of{" "}
+                <b>Seattle, Washington</b>
+              </p>
+              <p id="port">
+                Take a look at my <b>portfolio</b> below!
+              </p>
+            </div>
+            <div className="arrow-styles">
+              <img src={arrow} />
+            </div>
+            <div className="landing-image">
+              <img src={animatedMe} />
+            </div>
           </div>
         </div>
       </div>
