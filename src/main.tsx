6f2cc8b0b3d214e5c261496fa7550496e78d531d
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2024-07-29 18:17:22 -0700

    Trying something out
    I feel like there is too much to scroll through to get to what needs to be said...

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import Landing from "./Components/LandingPageComponents/LandingBody.tsx";
 import NavBar from "./Components/SharedComponents/NavBar.tsx";
 import AboutMe from "./Components/AboutMeComponents/AboutMe.tsx";
-import WhatIDo from "./Components/WhatIdo/WhatIDo.tsx";
+//import WhatIDo from "./Components/WhatIdo/WhatIDo.tsx";
 import TechStack from "./Components/TechStackComponents/TechStack.tsx";
 import Projects from "./Components/ProjectsComponents/Projects.tsx";
-import LikeWhatYouSee from "./Components/LikeWhatYouSeeComponents/LikeWhatYouSee.tsx";
+//import LikeWhatYouSee from "./Components/LikeWhatYouSeeComponents/LikeWhatYouSee.tsx";
 import ContactMe from "./Components/ContactComponents/ContactMe.tsx";
 import "./MainStyles.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -16,10 +16,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <NavBar />
     <Landing />
     <AboutMe />
-    <WhatIDo />
     <TechStack />
     <Projects />
-    <LikeWhatYouSee />
     <ContactMe />
   </React.StrictMode>
 );
