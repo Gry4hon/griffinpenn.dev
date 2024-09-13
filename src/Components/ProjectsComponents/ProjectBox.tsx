9eac01be6870088c44b3596f5eb461d8bd543794
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2024-09-12 19:37:24 -0700

    Project Page Changes
    The fly out cards just didn't sit right with me, so now we have just cards instead. And for future projects I can just use the ProjectBox component to add them to the page :)

--- src/Components/ProjectsComponents/ProjectBox.tsx
+++ src/Components/ProjectsComponents/ProjectBox.tsx
@@ -1,64 +1,27 @@
 import "./styles/ProjectBoxStyles.css";
-import { useState } from "react";
-import test from "./styles/Assets/github_logo.png";
+import github from "./styles/Assets/github_logo.png";
+import logo from "./styles/Assets/LOGO.png";
 
 function ProjectBox(props: any) {
-  const [isClicked, setIsClicked] = useState(false);
-
-  function ExpandDong() {
-    const backBody = document.getElementById(props.bodyKey);
-    const expandBar = document.getElementById(props.barKey);
-    const bio = document.getElementById(props.bioKey);
-    const technologies = document.getElementById(props.techKey);
-
-    if (!isClicked) {
-      backBody!.style.width = "1300px";
-      backBody!.style.backgroundColor = "#f6f6f6";
-      backBody!.style.border = "4px solid #6900ff";
-
-      technologies!.innerHTML = props.tech;
-      technologies!.style.marginLeft = "260px";
-
-      bio!.innerHTML = props.bio;
-      bio!.style.marginLeft = "260px";
-      bio!.style.marginTop = "10px";
-
-      expandBar!.style.marginLeft = "1250px";
-
-      setIsClicked(true);
-    } else if (isClicked) {
-      backBody!.style.width = "275px";
-      backBody!.style.backgroundColor = "#6900ff";
-      backBody!.style.border = "none";
-
-      technologies!.innerHTML = "";
-      technologies!.style.marginLeft = "0px";
-      technologies!.style.marginTop = "0px";
-
-      bio!.innerHTML = "";
-      bio!.style.marginLeft = "0px";
-      bio!.style.marginTop = "0px";
-
-      expandBar!.style.marginLeft = "230px";
-
-      setIsClicked(false);
-    }
-  }
   return (
     <>
-      <div className="container" id="box-cont">
+      <div className="container">
         <div id="box-body">
-          <p>{props.title}</p>
-          <a href={props.link}>
-            <img src={test} width="100px" height="100px" />
-          </a>
-        </div>
-        <div id={props.bodyKey} className="box-body-back">
-          <p id={props.techKey}></p>
-          <p id={props.bioKey}></p>
-        </div>
-        <div id={props.barKey} className="select-bar">
-          <button onClick={ExpandDong}></button>
+          <img className="project-picture-styles" src={logo}></img>
+          <div className="title-github">
+            <p className="title-styles">{props.title}</p>
+            <a href={props.link}>
+              <img src={github} className="githib-styles" />
+            </a>
+          </div>
+          <p className="bio-styles">{props.bio}</p>
+          <p className="tech-stack-title">Tech Used:</p>
+          <div className="tech-stack-contianer">
+            <img src={logo} />
+            <img src={logo} />
+            <img src={logo} />
+            <img src={logo} />
+          </div>
         </div>
       </div>
     </>
