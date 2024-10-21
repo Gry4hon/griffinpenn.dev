73ae16ce7346994117a07cf5d9b6904837d2c5ad
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2024-10-20 20:43:07 -0700

    about me

--- src/Components/AboutMeComponents/AboutMe.tsx
+++ src/Components/AboutMeComponents/AboutMe.tsx
@@ -10,7 +10,7 @@ function AboutMe() {
     <>
       <div onMouseEnter={ChangePage} id="aboutme-body">
         <div className="container">
-          <div className="aboutme-elements">
+          <div className="aboutme-elements-desktop">
             <div className="aboutme-text">
               <p>
                 I am a <b>passionate</b> developer, and recent college graduate,
@@ -35,6 +35,34 @@ function AboutMe() {
             </div>
             <div className="aboutme-image-carosel">
               <img src={graduate} alt="" />
+            </div>
+          </div>
+          <div className="aboutme-elements-mobile">
+            <div className="aboutme-header">
+              <p>
+                Who is <b className="colored-emp">Griffin Penn</b>
+              </p>
+              <div className="image-mobile">
+                <img src={graduate} alt="" />
+              </div>
+              <div className="aboutme-text">
+                <p>
+                  I am a <b>passionate</b> developer, and recent college
+                  graduate, who has the <b>drive</b> to make any project come to{" "}
+                  <b>life</b>. I have experience building{" "}
+                  <b>full stack web applications</b>,{" "}
+                  <b>virtual reality applications</b> and <b>hardware</b>, and
+                  have a deep fascination with <b>low level development</b>.
+                </p>
+                <br />
+                <p>
+                  My background <b>before</b> college was in fine arts, but
+                  through my major, a passion for <b>software development</b>{" "}
+                  grew. I have taken everyday since graduating learning more
+                  about <b>clean code</b>, <b>system design</b>, and best
+                  practices when developing <b>software</b>.
+                </p>
+              </div>
             </div>
           </div>
         </div>
