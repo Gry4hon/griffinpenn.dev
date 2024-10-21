ca6f82558e12fbdca8376777026156eea9df4818
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2024-10-20 21:03:19 -0700

    Projects

--- src/Components/ProjectsComponents/Projects.tsx
+++ src/Components/ProjectsComponents/Projects.tsx
@@ -27,7 +27,48 @@ function Projects() {
               have a public repository for you to explore on <b>GitHub</b>!
             </p>
           </div>
-          <div className="projects-container">
+          <div className="projects-container-desktop">
+            <ProjectBox
+              title="Project Vulcn"
+              link="https://github.com/Gry4hon/Project-Vulcn"
+              bio="This is an ongoing research and development project that seeks to understand how temperature can better immerse a user in virtual reality. This was done through developing custom hardware utilizing an Arduino microcontroller that was able to received and send a serial signal to the Unity game engine."
+              images={[
+                "/src/Components/ProjectsComponents/styles/Assets/Csharp_Logo.gif",
+                "/src/Components/ProjectsComponents/styles/Assets/Unity_Logo.gif",
+                "/src/Components/ProjectsComponents/styles/Assets/Arduino_Logo.gif",
+              ]}
+            />
+            <ProjectBox
+              title="Burger Rush"
+              bio="Burger Rush was a virtual reality local multiplayer game in which I served as the lead developer. The objective of the game was to work together making burgers for a lunch rush which involed prepping the burger, serving it, and cleaning the plates after the customers where done with them."
+              link="https://github.com/Gry4hon/Burger-Rush"
+              images={[
+                "/src/Components/ProjectsComponents/styles/Assets/Csharp_Logo.gif",
+                "/src/Components/ProjectsComponents/styles/Assets/Unity_Logo.gif",
+              ]}
+            />
+            <ProjectBox
+              title="Mockshop.com"
+              link="https://github.com/Gry4hon/Mockshop"
+              bio="Mockshop.com was a full-stack web app that emulated the functionality of an e-commerce website. I implemented a custom CRUD API for users to create accounts, have their carts saved, as well as a admin API to directly interact with the MySQL database."
+              images={[
+                "./styles/Assets/Csharp_Logo.gif",
+                "./styles/Assets/Unity_Logo.gif",
+                "./styles/Assets/Arduino_Logo.gif",
+              ]}
+            />
+            <ProjectBox
+              title="Swift Nav App"
+              bio="This was a collabrative effort to create a mobile app for Apple devices that would assist students with disabilities around Boise State campus. We utilized audatory, visual, and tactile queues that would tell the user to turn. The app also used image recognition to scan a set of doors of the building the user needed directions in."
+              link="https://github.com/Gry4hon/Swift-Nav-App"
+              images={[
+                "./styles/Assets/Csharp_Logo.gif",
+                "./styles/Assets/Unity_Logo.gif",
+                "./styles/Assets/Arduino_Logo.gif",
+              ]}
+            />
+          </div>
+          <div className="projects-container-mobile">
             <ProjectBox
               title="Project Vulcn"
               link="https://github.com/Gry4hon/Project-Vulcn"
